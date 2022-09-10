29b689b43623efc817568d3946a596b0bf7f8c42
Victor Moreira <93490417+victormoreira92@users.noreply.github.com>
2022-09-10 01:07:02 -0300

    Update SenhaView.js

--- assets/js/view/SenhaView.js
+++ assets/js/view/SenhaView.js
@@ -7,7 +7,8 @@ export class SenhaView{
             Fraca:['Fraca','url("../assets/img/fraco.jpg")','#ff8181'],
             Boa:['Boa','url("../assets/img/boa.jpg")','#ffae00'],
             Forte:['Forte','url("../assets/img/forte.jpg")','#0099ff'],
-            Muito_Forte:['Muito Forte','url("../assets/img/muito_forte.jpg")','#0000ff']
+            Muito_Forte:['Muito Forte','url("https://user-images.githubusercontent.com/93490417/189467967-33c18018-4fe4-4411-b53d-71750413ab98.jpg)
+")','#0000ff']
         }
         this.element = '';
         this.senha = senha;
