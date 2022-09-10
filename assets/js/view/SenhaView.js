087cd7b41290e213d32c4d711b8a41c12ed89d2d
Victor Moreira <93490417+victormoreira92@users.noreply.github.com>
2022-09-10 00:50:37 -0300

    Update SenhaView.js

--- assets/js/view/SenhaView.js
+++ assets/js/view/SenhaView.js
@@ -3,11 +3,11 @@ export class SenhaView{
     constructor(classificao,senha){
         this.classificao = classificao
         this.elementosView = {
-            Muito_Fraca: ['Muito Fraca','url("https://victormoreira92.github.io/geradorsenha/assets/img/muito_fraca.jpg")','#f00'],
-            Fraca:['Fraca','url("https://victormoreira92.github.io/geradorsenha/assets/img/fraco.jpg")','#ff8181'],
-            Boa:['Boa','url("https://victormoreira92.github.io/geradorsenha/assets/img/boa.jpg")','#ffae00'],
-            Forte:['Forte','url("https://victormoreira92.github.io/geradorsenha/assets/img/forte.jpg")','#0099ff'],
-            Muito_Forte:['Muito Forte','url("https://victormoreira92.github.io/geradorsenha/assets/img/muito_forte.jpg")','#0000ff']
+            Muito_Fraca: ['Muito Fraca','url("https://github.com/victormoreira92/geradorsenha/blob/main/assets/img/muito_fraca.jpg")','#f00'],
+            Fraca:['Fraca','url("https://github.com/victormoreira92/geradorsenha/blob/main/assets/img/fraco.jpg")','#ff8181'],
+            Boa:['Boa','url("https://github.com/victormoreira92/geradorsenha/blob/main/assets/img/boa.jpg")','#ffae00'],
+            Forte:['Forte','url("https://github.com/victormoreira92/geradorsenha/blob/main/assets/img/forte.jpg")','#0099ff'],
+            Muito_Forte:['Muito Forte','url("https://github.com/victormoreira92/geradorsenha/blob/main/assets/img/muito_forte.jpg")','#0000ff']
         }
         this.element = '';
         this.senha = senha;
